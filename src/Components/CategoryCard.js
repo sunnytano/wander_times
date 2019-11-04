ee0495d8cdd74d9ccbdfac7f08f9c50651afc847
Susan Tan <ssusantan9@gmail.com>
2019-11-04 16:01:30 -0500

    changed grid columns, fixed padding

--- src/Components/CategoryCard.js
+++ src/Components/CategoryCard.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Fragment } from 'react'
 import { Grid } from 'semantic-ui-react'
 
 class CategoryCard extends React.Component{
     render(){
-        
         return(
             <Grid divided='vertically'>
             <Grid.Row columns={2}>
-              <Grid.Column>
-              <div className='category-card-tech'>TECH</div>
+              <Grid.Column >
+              <div onClick={this.handleToggle} 
+                   className='category-card-tech'>TECH</div>
               </Grid.Column>
               <Grid.Column>
               <div className='category-card-travel'>TRAVEL</div>
               </Grid.Column>
-            </Grid.Row>
+            </Grid.Row>    
             </Grid>
         )
     }
