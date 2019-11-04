6d18f02c0cbef2989c067200a28fed403aa9e2e4
Susan Tan <ssusantan9@gmail.com>
2019-11-04 15:20:40 -0500

    css-search, categorycard

--- src/Components/CategoryCard.js
+++ src/Components/CategoryCard.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
 
 class CategoryCard extends React.Component{
     render(){
+        
         return(
-            <div>
-                
-            </div>
+            <Grid divided='vertically'>
+            <Grid.Row columns={2}>
+              <Grid.Column>
+              <div className='category-card-tech'>TECH</div>
+              </Grid.Column>
+              <Grid.Column>
+              <div className='category-card-travel'>TRAVEL</div>
+              </Grid.Column>
+            </Grid.Row>
+            </Grid>
         )
     }
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -27,7 +27,7 @@ class SearchBar extends React.Component{
             <div>
                 <form onSubmit={this.handleSubmit}>
                 <Fragment className="ui icon input">
-                    <Input 
+                    <Input id="search-bar"
                            onChange={this.handleChange}
                            placeholder="search articles"
                            name="newInput"
